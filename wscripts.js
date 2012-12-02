7d860b057e9aa57aacc845949d0cf4cc330d8143
Leo Tse <leotse1983@gmail.com>
2012-12-01 23:46:05 -0500

    Added pin details endpoint

--- wscripts.js
+++ wscripts.js
@@ -24,26 +24,26 @@ var request = require('request');
 // test endpoint to create pins //
 //////////////////////////////////
 
-var url = 'http://localhost:3000/api/pins?tid=50bac66812efe5180e000002'
-,	pin = {
-		author: '50b93b2e00b62ab60c000001',
-		title: 'yahoo pin',
-		url: 'http://www.yahoo.com',
-		picture: "http://l.yimg.com/a/i/mntl/ww/events/p.gif"
-	};
+// var url = 'http://localhost:3000/api/pins?tid=50bac66812efe5180e000002'
+// ,	pin = {
+// 		author: '50b93b2e00b62ab60c000001',
+// 		title: 'yahoo pin',
+// 		url: 'http://www.yahoo.com',
+// 		picture: "http://l.yimg.com/dh/ap/default/120910/yahoo_logo_ca.png"
+// 	};
 
-request({
-	method: 'post',
-	url: url,
-	form: pin
-}, output);
+// request({
+// 	method: 'post',
+// 	url: url,
+// 	form: pin
+// }, output);
 
 
 /////////////////////////////////////////
 // test endpoint to create annotations //
 /////////////////////////////////////////
 
-// var url = 'http://localhost:3000/api/annotations?pid=50bacbfe496587c90e000004'
+// var url = 'http://localhost:3000/api/annotations?pid=50bada65c8c98a0d100000ad'
 // ,	annotation = {
 // 		author: '50b93b2e00b62ab60c000001',
 // 		comment: 'I like this logo!'
